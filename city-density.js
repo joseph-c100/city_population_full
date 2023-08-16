9723050e9fb44b8a5156801c50c6560c160488ef
Joseph C <111119464+joseph-c100@users.noreply.github.com>
2023-08-16 12:02:52 +0100

    added src directory

--- city-density.js
+++ city-density.js
@@ -1,70 +1,75 @@
 import * as d3 from "d3";
 import * as Plot from "@observablehq/plot";
-import cityDensity from "/city-density.json?url";
-import urbanRural from "/urban-rural.json?url";
+
+import cityDensity from "/src/city-density.json";
+import urbanRural from "/src/urban-rural.json";
+
 
 
 // wait for the document to be fully loaded
-// document.addEventListener("DOMContentLoaded", function () {
-    // create a plot using the JSON data
-    const topCities = cityDensity.slice(0, 20);
+document.addEventListener("DOMContentLoaded", function () {
+        // create a plot using the JSON data
+        const topCities = cityDensity.slice(0, 20);
 
-    // first chart of city urban density
-    const plotFirst = Plot.plot({
-        style: {
-            backgroundColor: "#f8f8ec",
-            fontSize: "16"
-        },
-        y: {
-            grid: true,
-        },
-        marks: [
-            Plot.ruleY([0]),
-            Plot.barY(topCities, { x: "Entity", y: "Population density by city", sort: { x: "y", reverse: false }, fill:"#fa7f5e", tip:true }),
-            Plot.axisX({anchor: "bottom", label: null, tickRotate: -45}),
-            Plot.axisY({label: "Population density by city/km2"})
-        ],
-    });
-
-
-    // second chart of world urban/rural populations
-
-    const plotSecond = Plot.plot({
-        marginBottom: 80,
-        color: { legend: true },
-        style: {
-            backgroundColor: "#f8f8ec",
-            fontSize: "16"
-        },
-        y: {
-            label: "World population living in rural or urban areas",
-        },
-        x: {
-            grid: true,
-            tickFormat: "d"
-        },
-        marks: [
-            Plot.lineY(urbanRural, {x: "Year", y: "Urban population", stroke: "Red", strokeWidth: 2.5}),
-            Plot.lineY(urbanRural, {x: "Year", y: "Rural population", stroke: "Green", strokeWidth: 2.5}),
-            Plot.text(urbanRural, Plot.selectFirst({x: "Year", y: "Urban population", text: ["Urban"], textAnchor: "start", dx: 20, dy: -40})),
-            Plot.text(urbanRural, Plot.selectFirst({x: "Year", y: "Rural population", text: ["Rural"], textAnchor: "start", dx: 20, dy: -40})),
-            Plot.crosshairY(urbanRural, {y: "Urban population"}),
-        ]
-    })
+        // first chart of city urban density
+        const plotFirst = Plot.plot({
+            style: {
+                backgroundColor: "#f8f8ec",
+                fontSize: "16"
+            },
+            y: {
+                grid: true,
+            },
+            marks: [
+                Plot.ruleY([0]),
+                Plot.barY(topCities, { x: "Entity", y: "Population density by city", sort: { x: "y", reverse: false }, fill:"#fa7f5e", tip:true }),
+                Plot.axisX({anchor: "bottom", label: null, tickRotate: -45}),
+                Plot.axisY({label: "Population density by city/km2"})
+            ],
+        });
 
 
+        // second chart of world urban/rural populations
 
-
-    // append chart to div element
-    const chart1 = document.getElementById("migrationChart1");
-    chart1.append(plotFirst);
-
-    const chart2 = document.getElementById("migrationChart2");
-    chart2.append(plotSecond);
+        const plotSecond = Plot.plot({
+            marginBottom: 80,
+            color: { legend: true },
+            style: {
+                backgroundColor: "#f8f8ec",
+                fontSize: "16"
+            },
+            y: {
+                label: "World population living in rural or urban areas",
+            },
+            x: {
+                grid: true,
+                tickFormat: "d"
+            },
+            marks: [
+                Plot.lineY(urbanRural, {x: "Year", y: "Urban population", stroke: "Red", strokeWidth: 2.5}),
+                Plot.lineY(urbanRural, {x: "Year", y: "Rural population", stroke: "Green", strokeWidth: 2.5}),
+                Plot.text(urbanRural, Plot.selectFirst({x: "Year", y: "Urban population", text: ["Urban"], textAnchor: "start", dx: 20, dy: -40})),
+                Plot.text(urbanRural, Plot.selectFirst({x: "Year", y: "Rural population", text: ["Rural"], textAnchor: "start", dx: 20, dy: -40})),
+                Plot.crosshairY(urbanRural, {y: "Urban population"}),
+            ]
+        })
 
 
 
-// });
+
+        // append chart to div element
+        const chart1 = document.getElementById("migrationChart1");
+        chart1.append(plotFirst);
+
+        const chart2 = document.getElementById("migrationChart2");
+        chart2.append(plotSecond);
+
+
+
+});
+
+
+
 
 
 
